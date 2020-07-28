6f8d4a8f9ba0125f4e9547ec1c90325e10fc9cbc
tetsu4993 <tetsu19901103@gmail.com>
2020-07-28 17:19:25 +0900

    既読機能のサーバーサイドの記述

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_07_08_091833) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content"
+    t.boolean "cheacked"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
